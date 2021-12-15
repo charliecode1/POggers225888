0bf1cfd22227f6e484a1dafb2ecee0543838cd5e
charliecode1 <81428859+charliecode1@users.noreply.github.com>
2021-12-15 13:57:15 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,90 @@
+const progressBarContainer = document.querySelector("#progressBarContainer");
+const progressBar = document.querySelector("#progressBar");
+let totalPageHeight = document.body.scrollHeight - window.innerHeight;
+let debounceResize;
+
+window.addEventListener(
+  "scroll",
+  () => {
+    let newProgressHeight = window.pageYOffset / totalPageHeight;
+    progressBar.style.transform = `scale(1,${newProgressHeight})`;
+    progressBar.style.opacity = `${newProgressHeight}`;
+  },
+  {
+    capture: true,
+    passive: true
+  }
+);
+
+window.addEventListener("resize", () => {
+  clearTimeout(debounceResize);
+  debounceResize = setTimeout(() => {
+    totalPageHeight = document.body.scrollHeight - window.innerHeight;
+  }, 250);
+});
+
+progressBarContainer.addEventListener("click", (e) => {
+  let newPageScroll =
+    (e.clientY / progressBarContainer.offsetHeight) * totalPageHeight;
+  window.scrollTo({
+    top: newPageScroll,
+    behavior: "smooth"
+  });
+});
+
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+const navLogo = document.querySelector("#navbar__logo");
+
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+};
+
+menu.addEventListener("click", mobileMenu);
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+  const elem = document.querySelector(".highlight");
+  const homeMenu = document.querySelector("#home-page");
+  const aboutMenu = document.querySelector("#about-page");
+  const servicesMenu = document.querySelector("#services-page");
+  let scrollPos = window.scrollY;
+  // console.log(scrollPos);
+
+  // adds 'highlight' class to my menu items
+  if (window.innerWidth > 960 && scrollPos < 600) {
+    homeMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 1400) {
+    aboutMenu.classList.add("highlight");
+    homeMenu.classList.remove("highlight");
+    servicesMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 2345) {
+    servicesMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
+    return;
+  }
+
+  if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove("highlight");
+  }
+};
+
+window.addEventListener("scroll", highlightMenu);
+window.addEventListener("click", highlightMenu);
+
+//  Close mobile Menu when clicking on a menu item
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector(".is-active");
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.remove("active");
+  }
+};
+
+menuLinks.addEventListener("click", hideMobileMenu);
+navLogo.addEventListener("click", hideMobileMenu);
